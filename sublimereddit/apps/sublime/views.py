d8151c347b2b17a90b128bf57f75eb07b557c349
Adrian Espinosa <aespinosamoreno@gmail.com>
2012-11-04 10:43:57 +0100

    Added subreddit, selftext and next apge support

--- sublimereddit/apps/sublime/views.py
+++ sublimereddit/apps/sublime/views.py
@@ -1,5 +1,5 @@
 from django.conf import settings
-from django.shortcuts import render, render_to_response
+from django.shortcuts import render_to_response
 from django.http import HttpResponse
 import requests
 
@@ -9,4 +9,28 @@ def main_page(request):
     w = requests.get("http://www.reddit.com/.json")
     c = w.json
     print c
-    return render_to_response("sublime/index.html", {'c': c, 'app_name': settings.APPLICATION_NAME})
+    return render_to_response("sublime/index.html", {'c': c, 'app_name': settings.APPLICATION_NAME, 'name': 'home'})
+
+
+def comments_page(request, commentsuri):
+    pass
+
+
+def user_page(request, useruri):
+    pass
+
+
+def subreddit_page(request, subreddituri):
+    url = "http://www.reddit.com/r/" + subreddituri + "/.json"
+    print url
+    w = requests.get(url)
+    c = w.json
+    return render_to_response("sublime/index.html", {'c': c, 'app_name': settings.APPLICATION_NAME, 'name': subreddituri})
+
+
+def reddit_next_page(request, after):
+    url = "http://www.reddit.com/.json?after=" + after
+    print url
+    w = requests.get(url)
+    c = w.json
+    return render_to_response("sublime/index.html", {'c': c, 'app_name': settings.APPLICATION_NAME, 'name': after})
--- sublimereddit/urls.py
+++ sublimereddit/urls.py
@@ -7,6 +7,8 @@ from django.conf.urls import patterns, include, url
 urlpatterns = patterns('',
     # Examples:
     url(r'^$', 'sublimereddit.apps.sublime.views.main_page', name='home'),
+    url(r'^subreddit/(?P<subreddituri>[\w|\W]+)/$', 'sublimereddit.apps.sublime.views.subreddit_page', name="subreddit"),
+    url(r'^page/(?P<after>[\w|\W]+)/$', 'sublimereddit.apps.sublime.views.reddit_next_page', name="nextpage"),
     # url(r'^sublimereddit/', include('sublimereddit.foo.urls')),
 
     # Uncomment the admin/doc line below to enable admin documentation:
