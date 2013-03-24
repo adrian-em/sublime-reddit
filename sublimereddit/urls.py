0a0aa074faac9fd1d91a60ce6244529b1e721774
aesptux <aespinosamoreno@gmail.com>
2013-03-24 11:46:41 +0100

    Added robots.txt. It was throwing internal server errors

--- sublimereddit/urls.py
+++ sublimereddit/urls.py
@@ -1,4 +1,6 @@
 from django.conf.urls import patterns, include, url
+from django.views.generic.simple import direct_to_template
+
 
 # Uncomment the next two lines to enable the admin:
 # from django.contrib import admin
@@ -12,6 +14,8 @@ urlpatterns = patterns('',
     url(r'^comments/(?P<r>[\w|\W]+)/(?P<subreddit>[\w|\W]+)/(?P<comments>[\w|\W]+)/(?P<name>[\w|\W]+)/(?P<title>[\w|\W]+)/$',
     'sublimereddit.apps.sublime.views.comments_page', name="comments"),
     url(r'^settings/$', 'sublimereddit.apps.sublime.views.settings_page', name="settings"),
+    (r'^robots\.txt$', direct_to_template,
+     {'template': 'robots.txt', 'mimetype': 'text/plain'}),
     # url(r'^sublimereddit/', include('sublimereddit.foo.urls')),
 
     # Uncomment the admin/doc line below to enable admin documentation:
