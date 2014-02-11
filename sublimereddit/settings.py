08abcdc4ea7e3296d257bbe6093311027a65691d
Adrian Espinosa <aespinosamoreno@gmail.com>
2014-02-11 19:45:36 +0100

    Add allowed hosts

--- sublimereddit/settings.py
+++ sublimereddit/settings.py
@@ -10,7 +10,8 @@ ADMINS = (
 )
 
 MANAGERS = ADMINS
-
+ALLOWED_HOSTS = ['www.sublimereddit.com', 'sublimereddit.aesptux.com',
+                 '162.243.65.91']
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
